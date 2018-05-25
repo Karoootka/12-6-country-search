7dac398840a4f36b31c59ec237607105ad3cd6cf
Karolina Madej <kkarolinamadej@gmail.com>
2018-05-25 20:44:57 +0200

    Finish task

--- js/script.js
+++ js/script.js
@@ -1,24 +1,37 @@
 var url = 'https://restcountries.eu/rest/v1/name/';
-var countriesList = document.getElementById('countries');
+var countries = document.querySelector('.countries');
 
 document.getElementById('search').addEventListener('click', searchCountries);
 
-function searchCountries() {
-    var countryName = document.getElementById('country-name').value;
-    if(!countryName.length) countryName = 'Poland';
 
-    fetch(url + countryName)
-        .then(function(resp) {
-            return resp.json();
-        })
-        .then(showCountriesList);
+function searchCountries() {
+  var country = document.getElementById('country').value;
+  if(!country.length) countryName = 'Poland';
+
+  fetch(url + country)
+    .then(function(resp) {
+      document.querySelector('.table').classList.add('show');
+      return resp.json();
+    })
+    .then(showCountriesList);
 };
 
 function showCountriesList(resp) {
-  countriesList.innerHTML = '';
+  countries.innerHTML = '';
   resp.forEach(function(item) {
-    var liEl = document.createElement('li');
-    liEl.innerText = item.name + ' - capital: ' + item.capital;
-    countriesList.appendChild(liEl);
+    var newRow = document.createElement('tr');
+    var countryName = document.createElement('td');
+    var countryCapital = document.createElement('td');
+    var countryRegion = document.createElement('td');
+    countryName.innerText = item.name;
+    countryCapital.innerText = item.capital;
+    countryRegion.innerText = item.region;
+
+    countries.appendChild(newRow);
+    newRow.appendChild(countryName);
+    newRow.appendChild(countryCapital);
+    newRow.appendChild(countryRegion);
+
+
   })
 };
